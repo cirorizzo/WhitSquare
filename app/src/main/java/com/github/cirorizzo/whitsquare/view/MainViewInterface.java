ac6c3a0e6a9ceab2b69a5c2a9a35a769be90fcb5
cirorizzo <ciro.rizzo@gmail.com>
2016-02-02 16:29:07 +0000

    Implementation

--- app/src/main/java/com/github/cirorizzo/whitsquare/view/MainViewInterface.java
+++ app/src/main/java/com/github/cirorizzo/whitsquare/view/MainViewInterface.java
@@ -4,6 +4,9 @@ import com.github.cirorizzo.whitsquare.model.Venue;
 
 import java.util.List;
 
+/**
+ * View's interface
+ */
 public interface MainViewInterface {
     void setMessage(final String message);
     void setData(final List<Venue> venues);
