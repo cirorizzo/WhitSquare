df68f3f23da5bd8a75e2c9b11a5309031d8fe559
cirorizzo <ciro.rizzo@gmail.com>
2016-02-02 14:12:21 +0000

    Initial Commit

---
+++ app/src/test/java/com/github/cirorizzo/whitsquare/WhitSquarePresenterTest.java
@@ -0,0 +1,19 @@
+package com.github.cirorizzo.whitsquare;
+
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class WhitSquarePresenterTest {
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+}
